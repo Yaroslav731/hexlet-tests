dea46ada03fe5986788497244ce09021deddeb84
Ярослав Гончаров <yaroslav.x111@gmail.com>
2026-04-27 12:35:50 +0000

    add StringUtils and basic tests

---
+++ src/StringUtils.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StringUtils;
+
+function capitalize(string $text): string
+{
+	if ($text === '') {
+		return '';
+	}
+	$firstSymbol = mb_strtoupper($text[0]);
+	$restSubstring = mb_substr($text, 1);
+	return "{$firstSymbol}{$restSubstring}";
+}
